75375370ef825b43ff7e726de1ffca081f014fb7
Ryan LaRocca <ryanlarocca@Ryans-Mac-mini.local>
2026-03-08 12:20:44 -0700

    Initial macro tracker app

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'mt-cache-v1';
+const ASSETS = [
+  '/macro-tracker/',
+  '/macro-tracker/index.html',
+  '/macro-tracker/manifest.json',
+  '/macro-tracker/icon-192.png',
+  '/macro-tracker/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first strategy
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
